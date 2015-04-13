abd6576061ad047bbb2c2819b054de6eb1bed4a9
WangAnqi <waqthss2012@163.com>
2015-04-13 17:50:08 +0800

    has gun

--- DrawPerson.asm
+++ DrawPerson.asm
@@ -36,8 +36,8 @@ DrawPerson PROC USES eax ebx ecx, hdcbuffer:HDC, hperson:PTR Person
     mov speed,ebx
     mov bl,[eax].Person.alive
     mov alive,bl
-    ;mov bh,[eax].Person.hasGUN
-    ;mov hasGUN,bh
+    mov bl,[eax].Person.hasGun
+    mov hasGUN,bl
 
     .IF alive == ALIVE
         mov eax,headcenter_y
@@ -154,9 +154,102 @@ DrawPerson PROC USES eax ebx ecx, hdcbuffer:HDC, hperson:PTR Person
 
     INVOKE DrawLeg,hdcbuffer,waistpointx,waistpointy,legleftdegree,legspeed,ROTATE_DIRECTION_CLOCKWISE,direction
     INVOKE DrawLeg,hdcbuffer,waistpointx,waistpointy,legrightdegree,legspeed,ROTATE_DIRECTION_ANTICLOCKWISE,direction
+    
+    .IF hasGUN == HAS_GUN
+        INVOKE DrawRifle,hdcbuffer,headcenter_x,headcenter_y,direction
+    .ENDIF
 
     ret
 DrawPerson ENDP
+
+; x: headcenter_x, y: headcenter_y: direction: DIRECTION_LEFT or DIRECTION_RIGHT
+DrawRifle PROC USES eax edx, hdcbuffer: HDC, x: LONG, y: LONG, direction: SDWORD
+	LOCAL hThickPen: HPEN, oldPen: HPEN, hThinPen: HPEN, hMediumPen: HPEN
+	
+	RGB 0FFh, 0FFh, 0FFh
+	INVOKE CreatePen, PS_SOLID, 4, eax
+	mov hThickPen, eax
+	RGB 0FFh, 0FFh, 0FFh
+	INVOKE CreatePen, PS_SOLID, 2, eax
+	mov hThinPen, eax
+	RGB 0FFh, 0FFh, 0FFh
+	INVOKE CreatePen, PS_SOLID, 3, eax
+	mov hMediumPen, eax
+	
+	INVOKE SelectObject, hdcbuffer, hThickPen
+	mov oldPen, eax
+	
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		sub eax, 5
+	.ELSE
+		add eax, 5
+	.ENDIF
+	mov edx, y
+	add edx, 5
+	mov x, eax
+	mov y, edx
+	
+	INVOKE MoveToEx, hdcbuffer, x, y, NULL
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		add eax, 5
+	.ELSE
+		sub eax, 5
+	.ENDIF
+	mov edx, y
+	add edx, 10
+	INVOKE LineTo, hdcbuffer, eax, edx
+
+	INVOKE SelectObject, hdcbuffer, hMediumPen
+
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		add eax, 3
+	.ELSE
+		sub eax, 3
+	.ENDIF
+	mov edx, y
+	add edx, 6
+	INVOKE MoveToEx, hdcbuffer, eax, edx, NULL
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		sub eax, 6
+	.ELSE
+		add eax, 6
+	.ENDIF
+	mov edx, y
+	add edx, 6
+	INVOKE LineTo, hdcbuffer, eax, edx
+
+	INVOKE SelectObject, hdcbuffer, hThinPen
+	
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		add eax, 5
+	.ELSE
+		sub eax, 5
+	.ENDIF
+	mov edx, y
+	add edx, 10
+	INVOKE MoveToEx, hdcbuffer, eax, edx, NULL
+	mov eax, x
+	.IF direction == DIRECTION_LEFT
+		sub eax, 1
+	.ELSE
+		add eax, 1
+	.ENDIF
+	mov edx, y
+	add edx, 13
+	INVOKE LineTo, hdcbuffer, eax, edx
+	
+	INVOKE SelectObject, hdcbuffer, oldPen
+	INVOKE DeleteObject, hThickPen
+	INVOKE DeleteObject, hThinPen
+	INVOKE DeleteObject, hMediumPen
+
+	ret
+DrawRifle ENDP
 
 DrawHead PROC USES eax ebx ecx edx,hdcbuffer:HDC,headcenter_X:DWORD,headcenter_Y:DWORD
     LOCAL hbrush:HBRUSH, holdObject:HGDIOBJ
